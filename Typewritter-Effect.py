b20bbcd75d48e936911e14e923ccae36b40ed170
Badwolf926 <sam.wilde369@gmail.com>
2019-10-29 10:01:23 -0700

    Update Typewritter-Effect.py

--- Typewritter-Effect.py
+++ Typewritter-Effect.py
@@ -1,10 +1,14 @@
 from time import sleep
 from random import *
 
+#--------------------------------------------------------------#
+
 def write(text):
     for c in text:
         print(c, end='', flush=True)
         sleep(uniform(0.2, 0.1))
     print('')
-   
+  
+#--------------------------------------------------------------#
+
 write('Hello World!')
