e1cd0577b4adf4d30c108c0d7224d39ab6cfe2fa
Badwolf926 <sam.wilde369@gmail.com>
2020-02-23 01:34:17 +0000

    Fix bubble sort

--- sort.py
+++ sort.py
@@ -1,4 +1,4 @@
-def insertion(vals, place=0, ordering='ascending'):
+def bubble_sort(vals, place=0, pattern='ascending'):
     """Insertion sort algorithm to sort integers or floating point numbers in ascending or decending order.
     
     Arguments:
@@ -6,10 +6,10 @@ def insertion(vals, place=0, ordering='ascending'):
     
     Keyword Arguments:
         place {int} -- index of metadata if present, leave blank for no metadata (default: {0})
-        ordering {str} -- ordering pattern of digits, can be ascending or descending (default: {'ascending'})
+        pattern {str} -- ordering pattern of digits, can be ascending or descending (default: {'ascending'})
     
     Raises:
-        SyntaxError: if ordering specification is not recognized
+        SyntaxError: if ordering pattern is not recognized
         IndexError: if place value is out of range of metadata
         ValueError: if data to be sorted is not numbers
     
@@ -20,31 +20,33 @@ def insertion(vals, place=0, ordering='ascending'):
     # if no metadata attached, add metadata
     if place == 0:
         vals = [[v] for v in vals]
+    # check for metadata errors
     if place > len(vals[0])-1:
         raise IndexError('Metadata placement out of range of metadata tags')
     for i in vals:
         if type(i[place]) != (int or float):
             raise ValueError('Sortable data must be integer or floating point')
 
-    # while not sorted
+    # while not sorted,
     while ns != 0:
         # run though every digit
         for i in range(len(vals)):
             try:
-                # if ordering ascending, and digit more than next digit, swap with next digit
-                if ordering == 'ascending':
+                # if pattern ascending, and current digit more than next digit, swap with next digit
+                if pattern == 'ascending':
                     if vals[i][place] > vals[i+1][place]:
                         vals[i], vals[i+1] = vals[i+1], vals[i]
-                # if ordering decending, and digit less than next digit, swap with next digit
-                elif ordering == 'descending':
+                # if pattern decending, and  current digit less than next digit, swap with next digit
+                elif pattern == 'descending':
                     if vals[i][place] < vals[i+1][place]:
                         vals[i], vals[i+1] = vals[i+1], vals[i]
                 # otherwise leave be and move on
 
-                # if ordering type not recognized,
+                # if ordering pattern not recognized,
                 else:
-                    # kill it
+                    # raise an error
                     raise SyntaxError(f'Ordering pattern not recognized')
+
             # if at the end of the list
             except IndexError:
                 pass
@@ -52,11 +54,33 @@ def insertion(vals, place=0, ordering='ascending'):
         ns -= 1
     return vals
 
+class BubbleSort():
+    def ascending(self, vals):
+        ns = len(vals)
+        while ns > 0:
+            try:
+                for i in range(len(vals)):
+                    if vals[i] < vals[i+1]:
+                        vals[i], vals[i+1] = vals[i+1], vals[i]
+            except IndexError:
+                ns -= 1
+        return vals
+
+    def descending(self, vals):
+        ns = len(vals)
+        while ns > 0:
+            try:
+                for i in range(len(vals)):
+                    if vals[i] < vals[i+1]:
+                        vals[i], vals[i+1] = vals[i+1], vals[i]
+            except IndexError:
+                ns -= 1
+        return vals
 
 def shaker(vals):
     pass
 
-def bubble(vals):
+def insertion(vals):
     pass
 
 def quick(vals):
@@ -70,5 +94,6 @@ def tim(vals):
 
 
 unsorted = [(6, 3), (2, 5), (5, 2), (1, 1), (1, 4), (4, 0)]
-srtd = insertion(unsorted, 1)
-print(f'Insertion: {srtd}')
+bub = BubbleSort()
+srtd = bub.ascending(unsorted)
+print(f'Bubble Sort: {srtd}')
